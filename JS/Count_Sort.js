5fcf7ec4fd8ebfa03748ac3803661f500d88d145
Abdellatif Elaroussi <elaroussi@outlook.com>
2021-03-16 02:30:01 +0100

    Count Sort

--- JS/Count_Sort.js
+++ JS/Count_Sort.js
@@ -2,23 +2,18 @@ function countSort(arr) {
 	const max = Math.max(...arr);
 	const n = arr.length;
 	const count = Array(max + 1).fill(0);
-
 	for (let i = 0; i < n; i++) {
 		count[arr[i]]++;
 	}
-
 	for (let i = 1; i <= max; i++) {
 		count[i] = count[i] + count[i - 1];
 	}
-
 	const temp = [];
-	for (let i = 0; i < n; i++) {
+	for (let i = n - 1; i >= 0; i--) {
 		temp[--count[arr[i]]] = arr[i];
 	}
-
 	for (let i = 0; i < n; i++) {
 		arr[i] = temp[i];
 	}
-
 	return arr;
 }
